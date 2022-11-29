7a0738415b2cf07874b6d3a252164e906215682a
Valmiki Vishwakarma <you@example.com>
2022-11-29 08:38:13 +0530

    Handled dynamic wait.

--- Selenium4Nov29_2022/src/main/java/Basics/TestElement.java
+++ Selenium4Nov29_2022/src/main/java/Basics/TestElement.java
@@ -1,8 +1,13 @@
 package Basics;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,9 +17,21 @@ public class TestElement {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
+	//	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.gmail.com");
 		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
 		driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+		
+		By pwd=By.name("Passwd");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(pwd)).sendKeys("Password");
+		
+		driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+		By wrong=By.xpath("//span[contains(text(),'Wrong password')]");
+		
+		String txt=wait.until(ExpectedConditions.presenceOfElementLocated(wrong)).getText();
+		System.out.println(txt);
+		driver.quit();
 	}
 
 }
