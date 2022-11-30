8651a2a136a10fb4d0b6eb93a81691e9474ae013
Valmiki Vishwakarma <you@example.com>
2022-11-30 09:44:33 +0530

    Adding extent report code.

--- Selenium4Nov29_2022/src/main/java/testngLearning/TestExtentReport.java
+++ Selenium4Nov29_2022/src/main/java/testngLearning/TestExtentReport.java
@@ -1,12 +1,17 @@
 package testngLearning;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
@@ -29,37 +34,70 @@ public class TestExtentReport {
 		extent.setSystemInfo("Automation Test Engineer", "VV Automation Expert");
 		extent.setSystemInfo("Build No", "19.9.22");
 		extent.setSystemInfo("Organization", "ABC");
-		
 
 	}
-	
+
+	@Test
+	public void doLogout() {
+		test = extent.createTest("doLogout Test");
+		test.info("Entered username");
+		test.info("Entered password");
+		test.info("Clicked on sign button");
+		test.info("Verify page title");
+		test.skip("Login Test passed");
+		//Assert.assertTrue(true);
+
+	}
 	@Test
 	public void doLogin() {
-		test=extent.createTest("doLogin Test");
+		test = extent.createTest("doLogin Test");
 		test.info("Entered username");
 		test.info("Entered password");
 		test.info("Clicked on sign button");
 		test.info("Verify page title");
 		test.pass("Login Test passed");
 		Assert.assertTrue(true);
-		
+
 	}
-	
+
 	@Test
 	public void doRegistration() {
-		test=extent.createTest("doRegistration Test");
+		test = extent.createTest("doRegistration Test");
 		test.info("Entered username");
 		test.info("Entered password");
 		test.info("Clicked on sign button");
 		test.info("Verify page title");
 		test.fail("doRegistration Test passed");
 		Assert.fail();
-		
-		
+
 	}
+
+	@AfterMethod
+	public void updateResult(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			String methodName = result.getMethod().getMethodName();
+		Markup m=	MarkupHelper.createLabel(methodName.toUpperCase()+" - FAILED", ExtentColor.RED);
+			test.fail(m);
+			
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			String methodName = result.getMethod().getMethodName();
+			Markup m=	MarkupHelper.createLabel(methodName.toUpperCase()+" - SKIPPED", ExtentColor.AMBER);
+			test.skip(m);
+			//test.skip("User reg Skipped: " + methodName);
+		}
+
+		else if (result.getStatus() == ITestResult.SUCCESS) {
+			String methodName = result.getMethod().getMethodName();
+			//test.pass("User reg passed: " + methodName);
+			Markup m=	MarkupHelper.createLabel(methodName.toUpperCase()+" - PASSED", ExtentColor.GREEN);
+			test.pass(m);
+		}
+
+	}
+
 	@AfterTest
 	public void endReport() {
-		
+
 		extent.flush();
 	}
 
---
+++ Selenium4Nov29_2022/src/main/java/utilities/ExtentManager.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+
+public class ExtentManager {
+	
+	public static ExtentReports createInstance(String fileName) {
+		
+		
+		
+		
+		return extent;
+		
+	}
+
+}
